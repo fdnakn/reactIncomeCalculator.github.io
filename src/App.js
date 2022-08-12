2f4e9c31444a0d8882168fd8dc020c1e886d23c1
akin <akinfidancal@hotmail.com>
2022-08-12 15:11:48 +0300

    incometracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React , {useState,useEffect} from 'react';
+import Header from './components/Header'
+import IncomeForm from './components/IncomeForm'
 function App() {
+  const [income,setIncome] =useState([])
+  // bu bizim incomelarımız arrayi olacak.
+  const [totalIncome,setTotalIncome] = useState(0);
+
+  useEffect(()=>{
+    let total = 0;
+    for(let i = 0 ; i < income.length; i++){
+        total += parseInt(income[i].price);
+    }
+    setTotalIncome(total);
+    //burda çalıştığını göster
+  },[income])
+
+function deleteItem(id){
+ const newArr = income.filter(item => item.id !== id)
+ setIncome(newArr);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header totalIncome={totalIncome} />
+      <IncomeForm income={income} setIncome={setIncome}/>
+      <div className="incomeListContainer">
+      {income.map((item) => (
+
+        <div key={item.id} className="incomeList">
+
+        <div>{item.desc}</div>
+        <div>{item.price} TL</div>
+        <div>{item.date}</div>
+
+        <button onClick={(e) => deleteItem(item.id)}>Delete</button>
+
+        </div>
+
+      ))}
+      </div>
     </div>
   );
 }
